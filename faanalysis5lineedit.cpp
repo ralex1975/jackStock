8dd942f0e60bd948d68947fcca656dffb250ff1a
Ake Jacksen <ajn@bredband.net>
2016-02-15 23:39:30 +0100

    refacoration

---
+++ faanalysis5lineedit.cpp
@@ -0,0 +1,5 @@
+#include "faanalysis5lineedit.h"
+
+FAAnalysis5LineEdit::FAAnalysis5LineEdit()
+{
+}
---
+++ faanalysis5lineedit.h
@@ -0,0 +1,10 @@
+#ifndef FAANALYSIS5LINEEDIT_H
+#define FAANALYSIS5LINEEDIT_H
+
+class FAAnalysis5LineEdit
+{
+public:
+    FAAnalysis5LineEdit();
+};
+
+#endif // FAANALYSIS5LINEEDIT_H
