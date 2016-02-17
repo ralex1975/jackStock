2d315f595a696fc8025597c6d478bf72494aec62
Ake Jacksen <ajn@bredband.net>
2016-02-17 21:05:03 +0100

    add more finance ctrls

---
+++ src/guiUtil/guifinancectrls.h
@@ -0,0 +1,18 @@
+#ifndef GUIFINANCECTRLS_H
+#define GUIFINANCECTRLS_H
+
+#include <QWidget>
+
+class GuiFinanceCtrls : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit GuiFinanceCtrls(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // GUIFINANCECTRLS_H
